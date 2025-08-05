88152fc953c0f5a9e9db9eca907f72fc6cbc9459
chandankr111 <122672735+chandankr111@users.noreply.github.com>
2025-08-05 18:12:42 +0530

    medical-chatbot

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Chandan Kumar",
+    author_email="chandankr824142@gmail.com", 
+    packages=find_packages(),
+    install_requires=[
+       
+    ]
+)
